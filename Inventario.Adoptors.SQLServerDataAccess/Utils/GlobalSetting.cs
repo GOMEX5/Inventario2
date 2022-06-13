2d89f492a094a7927e8eb34a0cd1a86748f3510f
gomex <gomex@DESKTOP-BM3Q02O>
2022-06-13 09:03:11 -0600

    Agregar archivos de proyecto.

---
+++ Inventario.Adoptors.SQLServerDataAccess/Utils/GlobalSetting.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inventario.Adoptors.SQLServerDataAccess.Utils
+{
+    class GlobalSetting
+    {
+        public static string SqlServerConnectionString =
+                @"Data Source=DESKTOP-BM3Q02O;Initial Catalog=InventarioDB;Integrated Security=True;";
+    }
+}
